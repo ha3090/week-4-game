e90a6ec84300836700548b17f40a68fe40c4fd89
user <user@Hiras-MacBook.local>
2016-05-28 15:23:54 -0400

    js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,19 @@
-var aqua = Math.floor(Math.random() *12) + 1;
-var yellow = Math.floor(Math.random() *12) + 1;
-var red = Math.floor(Math.random() *12) + 1;
-var blue = Math.floor(Math.random() *12) + 1;
+var wins = 0;
+var losses = 0;
 
-document.getElementById("aqua").innerHTML = ;
+	document.getElementById("windisplay").innerHTML = wins;
+	document.getElementById("lossesdisplay").innerHTML = losses;	
+
+	var aqua = Math.floor(Math.random() *12) + 1;
+	var yellow = Math.floor(Math.random() *12) + 1;
+	var red = Math.floor(Math.random() *12) + 1;
+	var blue = Math.floor(Math.random() *12) + 1;
+
+document.getElementById("valueOfButton").innerHTML = Math.floor(Math.random() *12) + 1;
+
+
+
+		document.getElementById("aqua").innerHTML = Math.floor(Math.random() *12) + 1;
 
 
 
